1f35763780d5422a9b519dbd909f89b2cb9fcdb8
Phillip Verheyden <pverheyden@gmail.com>
2017-03-14 13:36:44 -0500

    First draft of working Hyperwallet API invocations

--- src/main/java/com/hyperwallet/RegisterController.java
+++ src/main/java/com/hyperwallet/RegisterController.java
@@ -3,12 +3,46 @@
  */
 package com.hyperwallet;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hyperwallet.clientsdk.Hyperwallet;
+import com.hyperwallet.clientsdk.model.HyperwalletUser;
+
+import javax.annotation.Resource;
 
 /**
  * 
  * 
  * @author Phillip Verheyden (phillipuniverse)
  */
+@Controller
+@RequestMapping("")
 public class RegisterController {
 
+    @Resource
+    protected Hyperwallet client;
+    
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody HyperwalletUser index() {
+        HyperwalletUser user = new HyperwalletUser();
+        user
+          .clientUserId("test-client-id-2")
+          .profileType(HyperwalletUser.ProfileType.INDIVIDUAL)
+          .firstName("Daffyd")
+          .lastName("y Goliath")
+          .email("testmail-1@hyperwallet.com")
+          .addressLine1("123 Main Street")
+          .city("Austin")
+          .stateProvince("TX")
+          .country("US")
+          .postalCode("78701");
+
+        HyperwalletUser createdUser = client.createUser(user);
+        return createdUser;
+    }
+    
 }
--- src/main/java/com/hyperwallet/UpbringApplication.java
+++ src/main/java/com/hyperwallet/UpbringApplication.java
@@ -1,7 +1,11 @@
 package com.hyperwallet;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hyperwallet.clientsdk.Hyperwallet;
 
 @SpringBootApplication
 public class UpbringApplication {
@@ -9,4 +13,19 @@ public class UpbringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UpbringApplication.class, args);
 	}
+	
+	@Value("${hyperwallet.user}")
+	String username;
+	
+	@Value("${hyperwallet.password}")
+	String password;
+	
+	@Value("${hyperwallet.program}")
+	String program;
+	
+	@Bean
+	public Hyperwallet client() {
+	    return new Hyperwallet(username, password, program);
+	}
+	
 }

80742a5348f5b2357f537d06626a324457df6a69
Phillip Verheyden <pverheyden@gmail.com>
2017-03-14 14:09:47 -0500

    Add working Twilio invocations

--- src/main/java/com/hyperwallet/UpbringApplication.java
+++ src/main/java/com/hyperwallet/UpbringApplication.java
@@ -1,3 +1,4 @@
+
 package com.hyperwallet;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -6,26 +7,40 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.hyperwallet.clientsdk.Hyperwallet;
+import com.twilio.Twilio;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class UpbringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UpbringApplication.class, args);
-	}
-	
-	@Value("${hyperwallet.user}")
-	String username;
-	
-	@Value("${hyperwallet.password}")
-	String password;
-	
-	@Value("${hyperwallet.program}")
-	String program;
-	
-	@Bean
-	public Hyperwallet client() {
-	    return new Hyperwallet(username, password, program);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(UpbringApplication.class, args);
+    }
+
+    @Value("${hyperwallet.user}")
+    String hyperwalletUsername;
+
+    @Value("${hyperwallet.password}")
+    String hyperwalletPassword;
+
+    @Value("${hyperwallet.program}")
+    String hyperwalletProgram;
+    
+    @Value("${twilio.username}")
+    String twilioUsername;
+    
+    @Value("${twilio.password}")
+    String twilioPassword;
+
+    @PostConstruct
+    public void twilioClient() {
+        Twilio.init(twilioUsername, twilioPassword);
+    }
+
+    @Bean
+    public Hyperwallet walletClient() {
+        return new Hyperwallet(hyperwalletUsername, hyperwalletPassword, hyperwalletProgram);
+    }
+
 }
